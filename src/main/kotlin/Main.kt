a99026bbe8c1287a2d0dbbb89cda7d816f81381e
Chris Athanas <chris@crowdskout.com>
2021-11-09 16:40:55 -0600

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,53 @@
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
+
+private fun log(msg:String) = println("[${Thread.currentThread().name}] $msg")
+
+class Main() {
+
+    fun main(args: Array<String>) {
+        // Try adding program arguments via Run/Debug configuration.
+        // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+        println("Program arguments: ${args.joinToString()}")
+        println("Num args=${args.size}")
+
+        runBlocking {
+            networkRequest()
+        }
+
+    }
+
+    private suspend fun networkRequest() {
+
+        val time = measureTimeMillis {
+            val job = GlobalScope.launch(Dispatchers.IO) {
+                runBlocking {
+                    log("Staring runBlocking")
+                    delay(1000)
+                    log("runBLocking...")
+                    delay(1000)
+                }
+
+                coroutineScope {
+                    log("Staring coroutineScope...")
+                    delay(1000)
+                    log("coroutingScope...")
+                    delay(1000)
+                }
+            }
+
+            job.join()
+            log("Done!")
+        }
+
+
+        println("Time: $time")
+
+    }
+
+}
+
+fun main(args: Array<String>) {
+//    val args2: Array<String> = Array(1) { "hello" }
+    Main().main(args)
+}
